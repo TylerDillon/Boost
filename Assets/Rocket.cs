9485d895c5b093da68f7bad343d466df2132d9e0
TylerDillon <t.dillon4884@gmail.com>
2019-02-05 17:14:18 -0800

    Rocket thrust and audio added

---
+++ Assets/Rocket.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource thrustAudio;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 50f;
+
+    // Start is called before the first frame update
+    void Start(){
+        rigidBody = GetComponent<Rigidbody>();
+        thrustAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        Thrust();
+        Rotate();
+    }
+
+    private void Rotate() {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A)) {
+           
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; //resume physics control
+    }
+
+    private void Thrust() {
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!thrustAudio.isPlaying) { // so it doesn't layer audio
+                thrustAudio.Play();
+            }
+        }
+        else {
+            thrustAudio.Stop();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                print("OK");
+                break;
+            default:
+                print("DEAD");
+                break;
+        }
+    }
+}
